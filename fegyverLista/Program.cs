8d72b66f63e71e160e2c63594c9df03a81d50ce5
Pádár.Csaba <Pdr.Csaba@C102-18>
2020-02-25 13:17:04 +0100

    Inventory made

--- fegyverLista/Program.cs
+++ fegyverLista/Program.cs
@@ -8,9 +8,17 @@ namespace fegyverLista
 {
     class Program
     {
+        enum fegyverek {Apa_Ökle, UZI, BFG9000, M4A1, kézigránát, lángszóró};
         static void Main(string[] args)
         {
-
+            fegyverek kézben = fegyverek.kézigránát;
+            List<fegyverek> inventory = new List<fegyverek>();
+            inventory.Add(fegyverek.UZI);
+            inventory.Add(fegyverek.lángszóró);
+            foreach (fegyverek item in inventory)
+            {
+                Console.WriteLine(item);
+            }
 
             Console.ReadKey();
         }
